82e3099ea71ec690293d40127fc1f975eba48aa4
mayazhang <mayazhang1241@gmail.com>
2025-09-19 20:03:49 -0400

    Add initial project setup with frontend (Tailwind CSS v4) and backend

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(5000, () => console.log("Server running on port 5000"));
+  })
+  .catch((err) => console.log(err));
